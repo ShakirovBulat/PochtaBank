efc78b8af24554e601595a02f44ac192421af830
Шакиров Булат Рустемович <211923@mck-ktits.ru>
2021-09-02 10:16:14 +0300

    Добавьте файлы проекта.

---
+++ PochtaBank/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PochtaBank
+{
+    class BankAccount
+    {
+        private double _Ballance;
+
+        public double Ballance{get ; set;}
+
+    }
+}
---
+++ PochtaBank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PochtaBank
+{
+    class Operations
+    {
+        public static void ShowBalance(BankAccount account)
+        {
+            System.Console.WriteLine(account.Ballance);
+        }
+        public static void Transaction(BankAccount accountSeller, BankAccount accountGetter, double summ )
+        {
+            accountSeller.Ballance -= summ;
+            accountGetter.Ballance += summ;
+            System.Console.WriteLine("Transaction Completed");
+        }
+    }
+}
